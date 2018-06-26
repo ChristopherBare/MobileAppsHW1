cdbfb2d4a11051e9e9b6e1748fc568137de93305
Christopher Bare <christopherbare@outlook.com>
2018-06-25 22:39:19 -0400

    Added Contact Adapter.

---
+++ app/src/main/java/com/example/homework_1/ContactAdapter.java
@@ -0,0 +1,36 @@
+package com.example.homework_1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class ContactAdapter extends ArrayAdapter<Contact> {
+
+
+    public ContactAdapter(Context context, int resource, List<Contact> objects) {
+        super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Contact contact = getItem(position);
+        convertView = LayoutInflater.from(getContext()).inflate(R.layout.activity_contact_item, parent, false);
+
+        TextView item_name = convertView.findViewById(R.id.textViewName);
+        TextView item_phone = convertView.findViewById(R.id.textViewPhone);
+//        ImageView imageView = convertView.findViewById(R.id.item_image);
+
+        //set the data from the contact object
+        item_name.setText(contact.getFirstName() + " " + contact.getLastName());
+        item_phone.setText(contact.getPhone());
+//        imageView.setImageResource(contact. picID);
+
+        return convertView;
+    }
+}
